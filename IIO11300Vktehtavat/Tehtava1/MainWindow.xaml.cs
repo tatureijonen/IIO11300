baa77344630b123e786d3decaf831bd5c4832a2d
tatureijonen <reisca.reijonmaa@gmail.com>
2016-01-19 12:48:25 +0200

    Tehtävä valmis

--- IIO11300Vktehtavat/Tehtava1/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/Tehtava1/MainWindow.xaml.cs
@@ -52,10 +52,49 @@ namespace Tehtava1
     private void btnClose_Click(object sender, RoutedEventArgs e)
     {
 
-    }
-  }
 
-  public class BusinessLogicWindow
+    }
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+           /*try
+            {
+                double result;
+                result = BusinessLogicWindow.CalculatePerimeter(1, 1);
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //yield to an user that everything okay
+                
+            }*/
+
+            double length = double.Parse(textBox_Length.Text);
+            double height = double.Parse(textBox_Height.Text);
+            /* textBox_Area.Text = (length * height).ToString(); */
+            double area1 = length * height;
+            textBox_Area.Text = area1.ToString();
+            double extrawidth = double.Parse(textBox_Width.Text)*2;
+            length = length - extrawidth;
+            height = height - extrawidth;
+            double area2 = length * height;
+            area2 = area1 - area2;
+            textBox_PerimeterArea.Text = area2.ToString();
+            length = length * 2;
+            height = height * 2;
+            textBox_Perimeter.Text = (length + height).ToString();
+
+
+
+
+        }
+    }
+
+    public class BusinessLogicWindow
     {
     /// <summary>
     /// CalculatePerimeter calculates the perimeter of a window
