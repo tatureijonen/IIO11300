e4ec4cdecbfbfc3e2263c1b33e7e87e45cf3be47
tatureijonen <reisca.reijonmaa@gmail.com>
2016-02-07 16:15:25 +0200

    UI valmis ja koodia aloitettu.

---
+++ IIO11300Vktehtavat/Tehtava4OudotOliotSM-Liigassa/BLSM-Liiga.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT.IIO11300
+{
+    class Liiga
+    {
+        private string etunimi;
+
+        public string Etunimi
+        {
+            get { return etunimi; }
+            set { etunimi = value; }
+        }
+
+        private string sukunimi;
+
+        public string Sukunimi
+        {
+            get { return sukunimi; }
+            set { sukunimi = value; }
+        }
+
+        private string siirtohinta;
+
+        public string Siirtohinta
+        {
+            get { return siirtohinta; }
+            set { siirtohinta = value; }
+        }
+
+        private string seura;
+
+        public string Seura
+        {
+            get { return seura; }
+            set { seura = value; }
+        }
+
+        #region CONSTRUCTORS
+        public Liiga()
+        {
+            etunimi = "Etunimi";
+            sukunimi = "Sukunimi";
+            siirtohinta = "0";
+            seura = "Seura";
+        }
+
+        public Liiga(string enimi, string snimi, string hinta, string sra)
+        {
+            this.etunimi = enimi;
+            this.sukunimi = snimi;
+            this.siirtohinta = hinta;
+            this.seura = sra;
+        }
+        #endregion
+
+        #region METHODS
+        public string KokoNimi
+        {
+            get
+            {
+                return etunimi + " " + sukunimi;
+            }
+        }
+
+        public string EsitysNimi
+        {
+            get
+            {
+                return etunimi + " " + sukunimi + ", " + seura;
+            }
+        }
+        #endregion
+    }
+}
--- IIO11300Vktehtavat/Tehtava4OudotOliotSM-Liigassa/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/Tehtava4OudotOliotSM-Liigassa/MainWindow.xaml.cs
@@ -12,17 +12,43 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using JAMK.IT.IIO11300;
+using Microsoft.Win32;
 
 namespace Tehtava4OudotOliotSM_Liigassa
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
     public partial class MainWindow : Window
     {
+        List<Liiga> pelaajat;
         public MainWindow()
         {
             InitializeComponent();
+            InitializeMyStuff();
+        }
+
+        public void InitializeMyStuff()
+        {
+            pelaajat = new List<Liiga>();
+        }
+
+        private void buttonLuoPelaaja_Click(object sender, RoutedEventArgs e)
+        {
+            Liiga pelaaja = new Liiga();
+            try
+            {
+                
+                listBox.Items.Add(pelaaja.EsitysNimi);
+            }
+            catch
+            {
+                MessageBox.Show("No go");
+            }
+            
+
         }
     }
 }
