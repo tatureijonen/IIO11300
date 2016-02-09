ae8eeb9ffa09ac2cb4317b4f31a97d537f9c1073
tatureijonen <reisca.reijonmaa@gmail.com>
2016-02-09 15:31:17 +0200

    Kohdat 1,2,3,8 tehtynä. Vielä loput niin hyvä tulee!

--- IIO11300Vktehtavat/Tehtava4OudotOliotSM-Liigassa/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/Tehtava4OudotOliotSM-Liigassa/MainWindow.xaml.cs
@@ -38,17 +38,76 @@ namespace Tehtava4OudotOliotSM_Liigassa
         private void buttonLuoPelaaja_Click(object sender, RoutedEventArgs e)
         {
             Liiga pelaaja = new Liiga();
-            try
+            bool valid = false;
+            bool pelaajavalid = true;
+            valid = textBoxValidator();
+            if(valid == true)
             {
-                
-                listBox.Items.Add(pelaaja.EsitysNimi);
+                pelaaja.Etunimi = txtBoxEtunimi.Text;
+                pelaaja.Sukunimi = txtBoxSukunimi.Text;
+                pelaaja.Siirtohinta = txtBoxHinta.Text;
+                pelaaja.Seura = comboBoxSeura.Text;
+                int index = pelaajat.Count;
+                for (int i = 0; i < index; i++)
+                {
+                    if(index == i)
+                    {
+                        break;
+                    }
+                    if(pelaaja.KokoNimi != pelaajat[i].KokoNimi)
+                    {
+                        pelaajavalid = true;
+                    }
+                    else
+                    {
+                        statusBar.Text = "Pelaaja on jo olemassa";
+                        pelaajavalid = false;
+                        break;
+                    }
+                }
+                if (pelaajavalid == true)
+                {
+                    statusBar.Text = "Pelaaja " + pelaaja.KokoNimi + " lisätty!";
+                    pelaajat.Add(pelaaja);
+                    listBoxNimi.Items.Add(pelaaja.EsitysNimi);
+                }
+                  
             }
-            catch
-            {
-                MessageBox.Show("No go");
-            }
-            
-
         }
+
+
+        public bool textBoxValidator()
+        {
+            if (string.IsNullOrWhiteSpace(txtBoxEtunimi.Text) ||
+                string.IsNullOrWhiteSpace(txtBoxSukunimi.Text) ||
+                string.IsNullOrWhiteSpace(txtBoxHinta.Text) ||
+                string.IsNullOrWhiteSpace(comboBoxSeura.Text))
+            {
+                MessageBox.Show("Syötä puuttuvat tiedot");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private void list_SelectionChanged (object sender, SelectionChangedEventArgs e)
+        {
+            if (listBoxNimi.SelectedValue == null) return;
+
+            var pelaaja = pelaajat.FirstOrDefault<Liiga>(p => p.EsitysNimi == listBoxNimi.SelectedValue.ToString());
+
+            if (pelaaja != null)
+            {
+                txtBoxEtunimi.Text = pelaaja.Etunimi;
+                txtBoxSukunimi.Text = pelaaja.Sukunimi;
+                txtBoxHinta.Text = pelaaja.Siirtohinta;
+                comboBoxSeura.SelectedValue = pelaaja.Seura;
+                
+            }
+        }
+
+       
     }
 }
