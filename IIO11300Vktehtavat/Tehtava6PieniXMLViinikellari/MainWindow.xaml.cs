4ab81d21175e206b8ce0b8b750d38646863b8d0e
tatureijonen <reisca.reijonmaa@gmail.com>
2016-02-23 21:36:31 +0200

    Comboboxiin tulee oikeat maat mutta ei suostu näyttämään niiden viinejä. Muuten toimii oikein.

--- IIO11300Vktehtavat/Tehtava6PieniXMLViinikellari/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/Tehtava6PieniXMLViinikellari/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml.Linq;
 using System.Linq;
+using System.Xml;
+
 
 namespace Tehtava6PieniXMLViinikellari
 {
@@ -27,15 +29,37 @@ namespace Tehtava6PieniXMLViinikellari
         public MainWindow()
         {
             InitializeComponent();
+            xe = XElement.Load(GetFileNime());
+            txtBlockFileName.Text = GetFileNime();
+
         }
 
         private void btnHaeViinit_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                xe = XElement.Load(GetFileNime());
-                txtBlockFileName.Text = GetFileNime();
-                dgWine.DataContext = xe.Elements("wine");
+                
+                dgWine.Items.Clear();
+
+                if (comboBoxMaa.SelectedItem == null)
+                {
+                    dgWine.DataContext = xe.Elements("wine");
+                }
+                else
+                {
+                    string selectedMaa = comboBoxMaa.SelectedItem.ToString();
+
+                    foreach (XElement viini in xe.Elements("wine"))
+                    {
+                        if(viini.Element("maa").Value == selectedMaa)
+                        {
+                            dgWine.Items.Add(viini);
+                        }
+                    }
+                }
+
+
+
             }
             catch (Exception ex)
             {
@@ -48,5 +72,32 @@ namespace Tehtava6PieniXMLViinikellari
         {
             return Tehtava6PieniXMLViinikellari.Properties.Settings.Default.XMLTiedosto;
         }
+
+         private void ComboBox_Loaded(object sender, RoutedEventArgs e)
+         {
+             List<string> maat = new List<string>();
+ 
+             try
+             {
+                 //maat comboboxiin, ei lisätä duplikaatteja      
+                 foreach (XElement maa in xe.Descendants("maa"))
+                 {
+                     if (!maat.Contains(maa.Value))
+                     {
+                         maat.Add(maa.Value);
+                     }
+                 }
+ 
+                 // ... Get the ComboBox reference.
+                 var comboBoxMaa = sender as ComboBox;
+ 
+                 // ... Assign the ItemsSource to the List.
+                 comboBoxMaa.ItemsSource = maat;
+             }
+             catch (Exception ex)
+             {
+                 MessageBox.Show(ex.Message);
+             }
+         }
     }
 }
