eb907c46c8c5295326c806ac1894a73093698b6f
Tatu Reijonen <H8510@student.jamk.fi>
2016-02-24 11:18:43 +0200

    Uutta hyvää irtomatskuu

---
+++ IIO11300Vktehtavat/H6/Hockey.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H6
+{
+    public class HockeyTeam
+    {
+        #region PROPERTIES
+
+        //huom public field ei kelpaa WPF:n databindingissa, pitää olla Property
+        public string Name { get; set; }
+        public string City { get; set; }
+        #endregion
+
+        #region CONSTRUCTORS
+        public HockeyTeam()
+        {
+            Name = " ";
+            City = "unknown";
+        }
+
+        public HockeyTeam(string name, string city)
+        {
+            Name = name;
+            City = city;
+        }
+        #endregion
+
+        public override string ToString()
+        {
+            return Name + "@" +City;
+            return base.ToString();
+        }
+    }
+
+    public class HockeyLeague
+    {
+        //perustetaan SM-Liiga, sisältää x kpl joukkueita
+        //Huom: Jos halutaan että databindaus huomaa automaattisesti muutokset
+        //kokoelmassa käytä ObservableCollection -kokoelmaa
+        List<HockeyTeam> teams = new List<HockeyTeam>();
+        public HockeyLeague()
+        {
+            teams.Add(new HockeyTeam("HIFK", "Helsinki"));
+            teams.Add(new HockeyTeam("JYP", "Jyväskylä"));
+            teams.Add(new HockeyTeam("Kalpa", "Kuopijo"));
+            teams.Add(new HockeyTeam("Sport", "Vaasa"));
+        }
+
+        //metodi joka palauttaa Liigan joukkueet
+        public List<HockeyTeam> GetTeams()
+        {
+            return teams;
+        }
+    }
+}
---
+++ IIO11300Vktehtavat/H6/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace H6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //moduulitason muuttujat
+        HockeyLeague smliiga;
+        List<HockeyTeam> joukkueet;
+        int clicked = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            FillMyCombobox();
+            smliiga = new HockeyLeague();
+            joukkueet = smliiga.GetTeams();
+        }
+
+        private void FillMyCombobox()
+        {
+            cbCourses2.Items.Add("IIO2010 Ohjelmistotuotanto");
+            cbCourses2.Items.Add("Pahamaineinen ruotsi");
+            cbCourses2.Items.Add("J2EE");
+        }
+
+        private void btnSetUser_Click(object sender, RoutedEventArgs e)
+        {
+            //luetaan app.configista UserName-asetus
+            txtUsername.Text = "Hello " + H6.Properties.Settings.Default.UserName;
+        }
+
+        private void btnBind_Click(object sender, RoutedEventArgs e)
+        {
+            myGrid.DataContext = joukkueet;
+        }
+
+        private void btnForward_Click(object sender, RoutedEventArgs e)
+        {
+            clicked++;
+            myGrid.DataContext = joukkueet[clicked];
+        }
+
+        private void btnBackward_Click(object sender, RoutedEventArgs e)
+        {
+            clicked--;
+            myGrid.DataContext = joukkueet[clicked];
+        }
+    }
+}
