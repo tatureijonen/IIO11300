efcf110a9015decc262087e3a04deb2e347793f2
tatureijonen <reisca.reijonmaa@gmail.com>
2016-03-11 21:10:09 +0200

    Ei toimi himasta käsin. Missä on vika?

---
+++ IIO11300Vktehtavat/Tehtava8ViiniasiakkaatFrom8/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tehtava8ViiniasiakkaatFrom8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHaeAsiakkaat_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                getData();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void getData()
+        {
+            using (SqlConnection conn =
+            new SqlConnection(Tehtava8ViiniasiakkaatFrom8.Properties.Settings.Default.DataSource))
+            {
+                string sql = "SELECT lastname FROM vCustomers";
+                SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+                DataTable dt = new DataTable("Japadapaduu");
+                da.Fill(dt);
+                dgData.DataContext = dt;
+                conn.Close();
+            }
+        }
+    }
+}
